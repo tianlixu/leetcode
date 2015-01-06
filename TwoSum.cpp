c7edae219568c8119905abb919764c584853b5d4
Alex Xu <tianli.xu@gmail.com>
2015-01-06 21:26:27 +0800

    added solution with map wich reduces the time comlexity with O(n)

--- TwoSum.cpp
+++ TwoSum.cpp
@@ -1,5 +1,23 @@
+/*
+ Source : https://oj.leetcode.com/problems/two-sum/
+ Author : Alex Xu
+ Date   : Jan 06, 2015
+  
+ Given an array of integers, find two numbers such that they add up to a specific target number.
+
+ The function twoSum should return indices of the two numbers such that they add up to the target,
+ where index1 must be less than index2.
+ Please note that your returned answers (both index1 and index2) are not zero-based.
+
+ You may assume that each input would have exactly one solution.
+
+ Input: numbers={2, 7, 11, 15}, target=9
+ Output: index1=1, index2=2
+ */
+
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
@@ -19,17 +37,35 @@ public:
 
         return v;
     }
+
+    vector<int> twoSum2(vector<int> &numbers, int target) {
+        std::vector<int> v;
+        std::map<int, int> m;
+
+        for (int i=0; i<numbers.size(); ++i) {
+            if (m.find(numbers[i]) == m.end()) {
+                m[target - numbers[i]] = i;
+            } else {
+                v.push_back(m[numbers[i]] + 1);
+                v.push_back(i+1);
+            }
+        }
+            
+        return v;
+    }
 };
 
 
 int main()
 {
-    int a[] = {2, 7, 9, 13};
+//    int a[] = {2, 7, 9, 13, -8, 20};
+    int a[] = {3, 2, 4};
     int length = sizeof(a) / sizeof(a[0]);
 
     std::vector<int> v(a, a+length);
     Solution s;
-    std::vector<int> result = s.twoSum(v, 20);
+    //std::vector<int> result = s.twoSum(v, 1);
+    std::vector<int> result = s.twoSum2(v, 6);
 
     cout << "index1=" << result[0] << " Index2=" << result[1] << endl;
                        
