6f2243862e9c38608fba88d9db17a149499587b9
Alex Xu <tianli.xu@gmail.com>
2015-03-04 16:43:34 +0800

    Create SortList.cpp

---
+++ SortList.cpp
@@ -0,0 +1,93 @@
+/*
+ Source : https://oj.leetcode.com/problems/sort-list/
+ Author : Alex Xu
+ Date   : Mar 04, 2015
+ Problem:
+Sort a linked list in O(n log n) time using constant space complexity.
+
+ Solution: MergeSort list.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *sortList(ListNode *head) {
+        if (head == NULL || head->next == NULL)
+            return head;
+
+        ListNode * middle = getMiddleNode(head);
+        ListNode * headR = NULL;
+        if (middle != NULL) {
+            headR = middle->next;
+            middle->next = NULL;
+        }
+
+        ListNode *headL = sortList(head);
+        headR = sortList(headR);
+        head = merge(headL, headR);
+
+        return head;
+    }
+    
+    ListNode *merge(ListNode *headL, ListNode *headR) {
+        if (headL == NULL)
+            return headR;
+        if (headR == NULL)
+            return headL;
+
+        if (headL->val > headR->val)
+            std::swap(headL, headR);
+
+        ListNode *head = headL; // make headL the head
+        headL = headL->next;
+
+        ListNode *current = head;
+        // link left list (headL) and right list(headR) to head
+        while (true) {
+            if (headL == NULL) {
+                current->next = headR;
+                break;
+            }
+
+            if (headR == NULL) {
+                current->next = headL;
+                break;
+            }
+            if (headL->val <= headR->val) {
+                current->next = headL;
+                current = headL;
+                headL = headL->next;
+            } else {
+                current->next = headR;
+                current = headR;
+                headR = headR->next;
+            }
+        }
+
+        return head;
+    }
+
+    ListNode *getMiddleNode(ListNode *head) {
+        if (head == NULL || head->next == NULL)
+            return head;
+            
+        ListNode *slow = head;
+        ListNode *fast = head->next;
+
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next; // step 1
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+
+
+};
