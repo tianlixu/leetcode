34cafcea6d6ff37162ab2d25d41d3024529372cb
Alex Xu <tianli.xu@gmail.com>
2015-03-10 22:48:25 +0800

    Update ReverseLinkeLlistII.cpp

--- ReverseLinkeLlistII.cpp
+++ ReverseLinkeLlistII.cpp
@@ -1,3 +1,20 @@
+/*
+ Source : https://leetcode.com/problems/reverse-linked-list-ii/
+ Author : Alex Xu
+ Date   : March 10, 2015
+ Problem: 
+ Reverse a linked list from position m to n. Do it in-place and in one-pass.
+
+For example:
+Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+
+return 1->4->3->2->5->NULL.
+
+Note:
+Given m, n satisfy the following condition:
+1 ≤ m ≤ n ≤ length of list.
+ */
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
