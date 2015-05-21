7fff382137476b597ac39f8e356cc2d6b2ac4d76
Alex Xu <tianli.xu@gmail.com>
2015-05-21 14:35:52 -0400

    Update 53-maximum-subarray.cpp
    added file header

--- 53-maximum-subarray.cpp
+++ 53-maximum-subarray.cpp
@@ -1,5 +1,9 @@
 /*
-
+ Source : https://leetcode.com/problems/maximum-subarray/
+ Author : Alex Xu
+ Date   : May 10, 2015
+ Problem:
+ 
 Find the contiguous subarray within an array (containing at least one number) which has the largest sum.
 
     For example, given the array [−2,1,−3,4,−1,2,1,−5,4],
