b7fabfe900e7c839e310103d1b99c1a3f48b76f8
Alex Xu <tianli.xu@gmail.com>
2015-05-28 19:24:36 -0400

    iterative solution

---
+++ 21-merge-two-sorted-lists.cpp
@@ -0,0 +1,50 @@
+/*
+ Source : https://leetcode.com/problems/merge-two-sorted-lists/
+ Author : Alex Xu
+ Date   : May 28, 2015
+ Problem:
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        if (l1 == nullptr)
+            return l2;
+        if (l2 == nullptr)
+            return l1;
+        
+        ListNode* head = nullptr;
+        if (l1->val < l2->val) {
+            head = l1;
+            l1 = l1->next;
+        } else {
+            head = l2;
+            l2 = l2->next;
+        }
+        
+        ListNode* p = head;
+        while (l1 != nullptr && l2 != nullptr) {
+            if (l1->val < l2->val) {
+                p->next = l1;
+                l1 = l1->next;
+            } else {
+                p->next = l2;
+                l2 = l2->next;
+            }
+            p = p->next;
+        }
+        p->next = (l1 == nullptr) ? l2 : l1;
+        
+        return head;
+    }
+};
+
