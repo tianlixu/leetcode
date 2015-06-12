c7fa932ea20aafe9cf012dc82a3f913ef917caa2
Alex Xu <tianli.xu@gmail.com>
2015-06-12 18:44:43 -0400

    recursive and iterative solution

---
+++ 226-invert-binary-tree.cpp
@@ -0,0 +1,66 @@
+/*
+  Source : 
+  Author : Alex Xu
+  Date   : June 10, 2015
+  Problem:
+  Invert a binary tree.
+
+       4
+     /   \
+    2     7
+   /  \   / \
+  1   3 6   9
+  
+  to
+       4
+     /   \
+    7     2
+   / \   / \
+  9   6 3   1
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    // Recursive solution
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr)
+            return nullptr;
+        
+        invertTree(root->left);
+        invertTree(root->right);
+        
+        std::swap(root->left, root->right);
+        
+        return root;
+    }
+
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr)
+            return nullptr;
+        
+        std::stack<TreeNode*> stk;
+        stk.push(root);
+        
+        while (!stk.empty()) {
+            TreeNode *node = stk.top();
+            stk.pop();
+            
+            std::swap(node->left, node->right);
+            if (node->left != nullptr)
+                stk.push(node->left);
+            if (node->right != nullptr)
+                stk.push(node->right);
+        }
+        
+        return root;
+    }    
+};
