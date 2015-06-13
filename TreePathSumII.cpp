9275aa71566c5d8cc8b0231f6b2a0660c6bf3e13
Alex Xu <tianli.xu@gmail.com>
2015-06-13 11:30:52 -0400

    refactoring

--- TreePathSumII.cpp
+++ TreePathSumII.cpp
@@ -1,25 +1,25 @@
 /*
- Source : https://leetcode.com/problems/path-sum-ii/
- Author : Alex Xu
- Date   : Mar 16, 2015
+  Source : https://leetcode.com/problems/path-sum-ii/
+  Author : Alex Xu
+  Date   : Mar 16, 2015
  
- Problem:
- Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+  Problem:
+  Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
 
-For example:
-Given the below binary tree and sum = 22,
-              5
-             / \
-            4   8
-           /   / \
-          11  13  4
-         /  \    / \
-        7    2  5   1
-return
-[
-   [5,4,11,2],
-   [5,8,4,5]
-]
+  For example:
+  Given the below binary tree and sum = 22,
+   5
+  / \
+  4   8
+  /   / \
+  11  13  4
+  /  \    / \
+  7    2  5   1
+  return
+  [
+  [5,4,11,2],
+  [5,8,4,5]
+  ]
 */
 
 /**
