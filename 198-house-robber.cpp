4a9236d14d09d51b846fbcac530d6d411aa87ea2
Alex Xu <tianli.xu@gmail.com>
2016-07-15 14:39:31 -0400

    added comments in detail for the dp solution

--- 198-house-robber.cpp
+++ 198-house-robber.cpp
@@ -10,7 +10,22 @@ Given a list of non-negative integers representing the amount of money of each h
 */
 
 /*
- dp(m) = max(a[m] + dp(m-2), dp(m-1))
+   0   1   2   3    i   5   6
+a [1] [2] [8] [13] [4] [2] [7]
+           |    |   |       
+        dp(i-2) |   |   
+             dp(i-1)|
+                    |dp(i) = max(a[i] + dp(i-2), dp(i-1))
+   
+1. If there is only one house, just take it: a[0]
+2. If there are two houses, then take the one with more money: max(a[0], a[1])
+3. If there are three houses or more, you can
+   either 
+       rob a[i], which means you cannot rob a[i-1], so you get: a[i] + dp(i-2)
+   or
+       do not rob a[i], then you get: dp(i-1)
+
+   Finally, the maximum for dp(i) is: max(a[i] + dp(i-2), dp(i-1))
 */
 class Solution {
 public:
