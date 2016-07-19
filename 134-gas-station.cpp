8c6233334d83ac2fe9a14e77417dd62fc7138cea
Alex Xu <tianli.xu@gmail.com>
2016-07-19 15:50:14 -0400

    added new solution for 134

--- 134-gas-station.cpp
+++ 134-gas-station.cpp
@@ -3,14 +3,14 @@
   Author : Alex Xu
   Date   : Jul 19, 2016
   Problem:
-There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
+  There are N gas stations along a circular route, where the amount of gas at station i is gas[i].
 
-You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to its next station (i+1). You begin the journey with an empty tank at one of the gas stations.
+  You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from station i to its next station (i+1). You begin the journey with an empty tank at one of the gas stations.
 
-Return the starting gas station's index if you can travel around the circuit once, otherwise return -1.
+  Return the starting gas station's index if you can travel around the circuit once, otherwise return -1.
 
-Note:
-The solution is guaranteed to be unique.
+  Note:
+  The solution is guaranteed to be unique.
 
   Tag: Greedy
 */
@@ -40,6 +40,43 @@ public:
             }
         }
         
+        return -1;
+    }
+};
+
+
+/*
+  Solution 2:
+  1. If total gas is greater than total cost, there is a solution,
+     a[i] = gas[i] - cost[i]
+  2. The start point must be the one that
+     2.1 is right after the minmum element in a[i]
+     2.2 but it must be a non-negative value
+    [-2, -2, -2, 3, 3]  => i=3
+    [-1, 2, -1] => i=2, but a[2] = -1, which is negative, so move on i++ until a[i] is non-negative, then i=1
+*/
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        for (int i=0; i<gas.size(); i++)
+            gas[i] -= cost[i];
+
+        int sum = 0;
+        int minIndex = 0;
+        for (int i=0; i< gas.size(); i++) {
+            sum += gas[i];
+            if (gas[i] <= gas[minIndex])
+                minIndex = i;
+        }
+
+        if (sum >= 0) { // there is a solution
+            int index = (minIndex + 1) % gas.size();
+            while (gas[index] < 0)
+                index ++;
+            return index;
+        }
+
+
         return -1;
     }
 };
