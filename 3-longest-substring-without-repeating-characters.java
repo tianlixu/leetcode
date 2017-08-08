17a3e23f8f47fcd1bafeb4410a2f9bc10ae1e4d5
tianlixu <tianli.xu@gmail.com>
2017-08-08 07:44:57 -0700

    added comments

--- 3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters.java
@@ -11,21 +11,26 @@ Greedy algorithm, Time O(n), Space O(1)
 
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
-        // substring s[i, j] is the answer
+        // substring s[i, j] is the answer, and ret is its length "ret = j-i+1"
         int i = 0;
         int j = 0;
-
         int ret = 0;
+
+        // HashMap saves a character(key) and its index
         HashMap<Character, Integer> m = new HashMap<Character, Integer>();
 
         while (j < s.length()) {
             if (m.containsKey(s.charAt(j))) {
+                /* If s[j] has a duplicate in the range [i,j) with index j' :
+                 *     j' = m.get(s.charAt(j))
+                 * skip all the elements in the range [i,j'] and move i to j'+1 directly.
+                 * i and j moves from left to right, will never move back.
+                 */
                 i = Math.max(m.get(s.charAt(j)) + 1, i);
             }
 
             ret = Math.max(ret, j-i+1);
             m.put(s.charAt(j), j);
-
             j++;
         }
 
