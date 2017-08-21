c4fd3d97dc194bf43e2c9c88d915a3f9f3626db3
Alex Xu <tianli.xu@gmail.com>
2017-08-21 10:09:08 -0400

    indent

--- 438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string.java
@@ -3,104 +3,116 @@
   Author : Alex Xu
   Date   : August 16, 2017
   Problem:
-Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
+  Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
 
-Strings consists of lowercase English letters only and the length of both strings s and p will not be larger than 20,100.
+  Strings consists of lowercase English letters only and the length of both strings s and p will not be larger than 20,100.
 
-The order of output does not matter.
+  The order of output does not matter.
 
-Example 1:
+  Example 1:
 
-Input:
-s: "cbaebabacd" p: "abc"
+  Input:
+  s: "cbaebabacd" p: "abc"
 
-Output:
-[0, 6]
+  Output:
+  [0, 6]
 
-Explanation:
-The substring with start index = 0 is "cba", which is an anagram of "abc".
-The substring with start index = 6 is "bac", which is an anagram of "abc".
-Example 2:
+  Explanation:
+  The substring with start index = 0 is "cba", which is an anagram of "abc".
+  The substring with start index = 6 is "bac", which is an anagram of "abc".
+  Example 2:
 
-Input:
-s: "abab" p: "ab"
+  Input:
+  s: "abab" p: "ab"
 
-Output:
-[0, 1, 2]
+  Output:
+  [0, 1, 2]
 
-Explanation:
-The substring with start index = 0 is "ab", which is an anagram of "ab".
-The substring with start index = 1 is "ba", which is an anagram of "ab".
-The substring with start index = 2 is "ab", which is an anagram of "ab".
+  Explanation:
+  The substring with start index = 0 is "ab", which is an anagram of "ab".
+  The substring with start index = 1 is "ba", which is an anagram of "ab".
+  The substring with start index = 2 is "ab", which is an anagram of "ab".
 
 */
 
-    public class Solution {
-        public List<Integer> findAnagrams(String s, String p) {
-            List<Integer> ret = new ArrayList<>();
-            if (s == null || p == null || s.length() == 0 || p.length() == 0 || s.length() < p.length()) {
-                return ret;
-            }
-
-            int[] h = new int[256];
-            for (char c : p.toCharArray()) {
-                h[c] ++;
-            }
-
-            int left = 0;
-            int right = 0;
-            int count = 0;
-            while (right < s.length()) {
-                h[s.charAt(right)] --;
-                if (h[s.charAt(right)] >= 0) {
-                    count ++;
-                }
-                right ++;
-
-                if (count == p.length()) {
-                    ret.add(left);
-                }
-
-                if (right - left == p.length()) {
-                    if (h[s.charAt(left)] >= 0) {
-                        count --;
-                    }
-
-                    h[s.charAt(left)] ++;
-                    left ++;
-                }
-            }
-
+public class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> ret = new ArrayList<>();
+        if (s == null || p == null || s.length() == 0 || p.length() == 0 || s.length() < p.length()) {
             return ret;
         }
-    }
 
-    public class Solution {
-        public List<Integer> findAnagrams(String s, String p) {
-            // Key: character, Value: counter of its appearance
-            HashMap<Character, Integer> m = new HashMap<Character, Integer>();
+        // hash array
+        int[] h = new int[256];
+        for (char c : p.toCharArray()) {
+            h[c] ++;
+        }
 
-            for (int i=0; i< p.length(); i++) {
-                if (m.containsKey(p.charAt(i))) {
-                    m.put(p.charAt(i), m.get(p.charAt(j)) + 1);
-                } else {
-                    m.put(p.charAt(i), 1);
-                }
+        // sliding window [left, right]
+        int left = 0;
+        int right = 0;
+        int count = 0;
+
+        while (right < s.length()) {
+            // move one char into the window from right, decrease its value by one
+            h[s.charAt(right)] --;
+            if (h[s.charAt(right)] >= 0) {
+                // if this char still belongs to 'p'
+                count ++;
             }
 
-            HashMap<Character, Integer> m2 = new HashMap<Character, Integer>();
-            m2.putAll(m);
+            // find a solution
+            if (count == p.length()) {
+                ret.add(left);
+            }
 
-            for (int i=0, int j=0; j<s.length(); j++) {
-                if (! m2.containsKey(s.charAt(j))) {
-                    continue;
+            if (right - left == p.length() - 1) {
+                // move one char out of the window from left
+                if (h[s.charAt(left)] >= 0) {
+                    // if this char belongs to 'p'
+                    count --;
                 }
 
-                int v = m2.get(s.charAt(j));
-                if (v == 0) {
-                    ++i;
-                }
-                
+                // recover the hash value
+                h[s.charAt(left)] ++;
+                // adjust window, moving one char forward from left
+                left ++;
+            }
+
+            // adjust window, moving one char forward from right
+            right ++;
+        }
+
+        return ret;
+    }
+}
+
+public class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        // Key: character, Value: counter of its appearance
+        HashMap<Character, Integer> m = new HashMap<Character, Integer>();
+
+        for (int i=0; i< p.length(); i++) {
+            if (m.containsKey(p.charAt(i))) {
+                m.put(p.charAt(i), m.get(p.charAt(j)) + 1);
+            } else {
+                m.put(p.charAt(i), 1);
             }
         }
+
+        HashMap<Character, Integer> m2 = new HashMap<Character, Integer>();
+        m2.putAll(m);
+
+        for (int i=0, int j=0; j<s.length(); j++) {
+            if (! m2.containsKey(s.charAt(j))) {
+                continue;
+            }
+
+            int v = m2.get(s.charAt(j));
+            if (v == 0) {
+                ++i;
+            }
+                
+        }
     }
+}
