4c9148f7ed5edb846c14a6ea78bcdb4e227049c9
Alex Xu <tianli.xu@gmail.com>
2017-08-22 20:58:44 -0400

    new solution

--- 209-minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum.java
@@ -30,6 +30,10 @@ class Solution {
         int sum = 0;
 
         while (right < nums.length) {
+            /*
+             * For each loop, either we increase the window if the current window is not a solution
+             * or we shrink the window if the current window is a solution
+             */
             if (sum < s) {
                 sum += nums[right];
             } else {
@@ -44,6 +48,40 @@ class Solution {
             }
         }
 
+        return ret == Integer.MAX_VALUE ? 0 : ret;
+    }
+}
+
+class Solution {
+    public int minSubArrayLen(int s, int[] nums) {
+        int ret = Integer.MAX_VALUE;
+        
+        // sliding window
+        int left = 0;
+        int right = 0;
+        
+        // condition
+        int sum = 0;
+        
+        while (right < nums.length) {
+            // for each loop, we increase the window until we get a solution
+            sum += nums[right];
+            
+            // once the current window is a solution, need to shrink the window from left
+            // until the current window is not a solution any more
+            while (sum >= s) {
+                // the current window is a solution
+                ret = Math.min(ret, right - left + 1);
+                
+                // shrink the window from left
+                sum -= nums[left];
+                left ++;
+            }
+            
+            // increase window
+            right ++;
+        }
+        
         return ret == Integer.MAX_VALUE ? 0 : ret;
     }
 }
