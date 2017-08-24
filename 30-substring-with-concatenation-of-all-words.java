06d953ea126b8191a06d8454858b4a0def4666d5
Alex Xu <tianli.xu@gmail.com>
2017-08-24 10:18:35 -0400

    fixed sliding window

--- 30-substring-with-concatenation-of-all-words.java
+++ 30-substring-with-concatenation-of-all-words.java
@@ -1,57 +1,74 @@
+/*
+  Source : https://leetcode.com/problems/substring-with-concatenation-of-all-words/description/
+  Author : Alex Xu
+  Date   : August 24, 2017
+  Problem:
+  You are given a string, s, and a list of words, words, that are all of the same length. Find all starting indices of substring(s) in s that is a concatenation of each word in words exactly once and without any intervening characters.
+
+  For example, given:
+  s: "barfoothefoobarman"
+  words: ["foo", "bar"]
+
+  You should return the indices: [0,9].
+  (order does not matter).
+
+*/
+
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
         List<Integer> ret = new ArrayList<Integer>();
-        
-        int counter = 0;
-        
-        // hashmap <char, counter>
-        HashMap<Character, Integer> m = new HashMap<Character, Integer>();
+
+        if (s == null || s.isEmpty() || words == null || words.length == 0) {
+            return ret;
+        }
+
+        int size = 0; // fixed window size for each try
+        HashMap<String, Integer> m = new HashMap<String, Integer>();
         for (String word : words) {
-            for (char c : word.toCharArray()) {
-                if (!m.containsKey(c)) {
-                    m.put(c, 1);
-                } else {
-                    m.put(c, m.get(c) + 1);
-                }
+            if (word == null || word.isEmpty()) {
+                continue;
             }
-            
-            counter += word.length();
+
+            if (!m.containsKey(word)) {
+                m.put(word, 1);
+            } else {
+                m.put(word, m.get(word) + 1);
+            }
+
+            size += word.length();
         }
-        
-        // sliding window
+
+        if (size == 0 || s.length() < size) {
+            return ret;
+        }
+
+        // sliding window, fixed size
         int left = 0;
-        int right = 0;
-        
-        // condition for a solution
-        int count = 0;
-        
-        while (right < s.length()) {
-            char c = s.charAt(right);
-            if (m.containsKey(c)) {
-                m.put(c, m.get(c) - 1);
-                if (m.get(c) >= 0) {
-                    count ++;
+
+        int length = words[0].length(); // word length
+        while (s.length() - left >= size) {
+            HashMap<String, Integer> m2 = new HashMap<String, Integer>(m);
+
+            for (int i=0; i<words.length; i++) {
+                String aWord = s.substring(left + i*length, left + (i+1)*length);
+                if (!m2.containsKey(aWord)) {
+                    break;
+                }
+
+                if (m2.get(aWord) <= 0) {
+                    break;
+                }
+
+                m2.put(aWord, m2.get(aWord) - 1);
+                if (i + 1 == words.length) { // find a solution
+                    ret.add(left);
                 }
             }
-            
-            if (count == counter) {
-                ret.add(left);
-            }
-            
-            if (right - left + 1 == counter) {
-                c = s.charAt(left);
-                if (m.containsKey(c)) {
-                    if (m.get(c) >= 0) {
-                        count --;
-                    }
-                    m.put(c, m.get(c) + 1);
-                }
-                left ++;
-            }
-            
-            right ++;
+
+            left ++;
         }
-        
+
         return ret;
     }
 }
+
