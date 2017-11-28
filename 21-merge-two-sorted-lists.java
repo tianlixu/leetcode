362a6847b617aedd15ddc4203ce32c6281c1b0f6
Alex Xu <tianli.xu@gmail.com>
2017-11-28 14:17:46 -0500

    iterative solution using dummy head

--- 21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists.java
@@ -15,6 +15,27 @@ Merge two sorted linked lists and return it as a new list. The new list should b
  * }
  */
 public class Solution {
+    // iterative, using dummy head
+    public class Solution {
+        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            ListNode dummy = new ListNode(0);
+            ListNode tail = dummy;
+
+            while (l1 != null && l2 != null) {
+                if (l1.val < l2.val) {
+                    tail.next = l1;
+                    l1 = l1.next;
+                } else {
+                    tail.next = l2;
+                    l2 = l2.next;
+                }
+                tail = tail.next;
+            }
+            tail.next = (l1 == null) ? l2: l1;
+
+            return dummy.next;
+        }
+    }
 
     // iterative solution, Runtime: 15 ms
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
