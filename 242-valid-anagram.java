61373f9177d25c8cf789084a53c8845e5a2f48b4
Alex Xu <tianli.xu@gmail.com>
2018-02-15 13:53:31 -0500

    sort and compare

--- 242-valid-anagram.java
+++ 242-valid-anagram.java
@@ -74,5 +74,20 @@ public class Solution {
         }
         return true;
     }
+
+    /**
+     * sort s and t then compare
+     * Time O(nlogn)
+     */
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        return Arrays.equals(str1, str2);
+    }
 }
 
