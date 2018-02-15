b91d5f288fbfa824e7c4d177adf5c3af9dadbf43
Alex Xu <tianli.xu@gmail.com>
2018-02-15 13:40:56 -0500

    solution with HashMap

--- 242-valid-anagram.java
+++ 242-valid-anagram.java
@@ -33,14 +33,46 @@ public class Solution {
         
         return alphabet;
     }
+
+    /**
+     *
+     * Solution 2 with only one HashMap:
+     * key is the s[i]/t[i]
+     * value: for s[i], ++1; for t[i], --1
+     * 
+     * if all values are 0, then return true; otherwise return false
+     * 
+     */
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> m = new HashMap<Character, Integer>();
+
+        for (int i=0; i<s.length(); i++) {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+
+            if (m.get(sChar) == null) {
+                m.put(sChar, 1); // default 1
+            } else {
+                m.put(sChar, m.get(sChar) + 1);
+            }
+
+            if (m.get(tChar) == null) {
+                m.put(tChar, -1); // default -1
+            } else {
+                m.put(tChar, m.get(tChar) - 1);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> entry : m.entrySet()) {
+            if (entry.getValue() != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
-/**
- *
- * Solution 2 with only one HashMap:
- * key is the s[i]/t[i]
- * value: for s[i], ++1; for t[i], --1
- * 
- * if all values are 0, then return true; otherwise return false
- * 
- */
