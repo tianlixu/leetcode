b35042b71abb4e9f2c4573971d6e227737b3bf8e
Alex Xu <tianli.xu@gmail.com>
2018-02-16 09:51:12 -0500

    polished a little bit

--- 64-minimum-path-sum.java
+++ 64-minimum-path-sum.java
@@ -42,25 +42,26 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         
-        int[][] sum = new int[m][n];
+        int[][] dp = new int[m][n];
         
-        sum[0][0] = grid[0][0];
-        
+        dp[0][0] = grid[0][0];
+
+        // initialize the 1st column, you can only move downward
         for (int i=1; i<m; i++) {
-            sum[i][0] = grid[i][0] + sum[i-1][0];
+            dp[i][0] = dp[i-1][0] + grid[i][0];
         }
-        
+        // initialize the 1st row, you can only move rightward
         for (int j=1; j<n; j++) {
-            sum[0][j] = grid[0][j] + sum[0][j-1];
+            dp[0][j] = dp[0][j-1] + grid[0][j];
         }
         
         for (int i=1; i<m; i++) {
             for (int j=1; j<n; j++) {
-                sum[i][j] = grid[i][j] + Math.min(sum[i-1][j], sum[i][j-1]);
+                dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
             }
         }
         
-        return sum[m-1][n-1];
+        return dp[m-1][n-1];
     }
 }
 
