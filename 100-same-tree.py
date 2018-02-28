bc70e9dbb45f7e5a1df60ac6b2ad24deb9fc9c1c
Alex Xu <tianli.xu@gmail.com>
2018-02-28 09:51:26 -0500

    tree to array

--- 100-same-tree.py
+++ 100-same-tree.py
@@ -30,3 +30,38 @@ class Solution:
             return False
         
         return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+"""
+convert the tree to an array of numbers then compare the array
+Note: leaf node will be save as [leaf_node.val, '/', '/'], e.g.
+    tree [1, 0] -> [1, 0, '/', '/', '/']
+while 
+    tree [1, null] -> [1, '/', '/']
+"""
+class Solution:
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        return self.treeToArray(p) == self.treeToArray(q)
+    
+    def treeToArray(self, root):
+        """
+        :type root: root TreeNode
+        :rtype: []
+        """
+        ret = []
+        if root:
+            ret.append(root.val)
+            ret += self.treeToArray(root.left)
+            ret += self.treeToArray(root.right)
+        else:
+            ret.append('/')
+        
+        return ret
+
+"""
+Solution 3: using stack/queue for dfs/bfs
+"""    
