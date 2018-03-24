729f04d8acb29d17b876d3dcf12319856e46b3c1
Alex Xu <tianli.xu@gmail.com>
2018-03-23 23:36:49 -0400

    in-order solution

---
+++ 230-kth-smallest-element-in-a-bst.java
@@ -0,0 +1,62 @@
+/**
+  Source : https://leetcode.com/problems/binary-tree-inorder-traversal/
+  Author : Alex Xu
+  Date   : Mar 23, 2018
+  Problem:
+Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+
+    Note:
+    You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+
+Follow up:
+What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
+
+Hint: in-order traversal a bst would create a sorted array. 
+      Similar problem #94: https://leetcode.com/problems/binary-tree-inorder-traversal/
+*/
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+import java.util.AbstractMap;
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {        
+        // in-order traversal
+        // Map.Entry is something like a tuple/pair in python                                                            
+        Stack<Map.Entry<TreeNode, Boolean>> stack = new Stack();
+        TreeNode node = root;
+        boolean visited = false;
+        Map.Entry<TreeNode, Boolean> entry = new AbstractMap.SimpleEntry(node, visited);
+        stack.push(entry);
+
+        while(!stack.empty()) {
+            entry = stack.pop();
+            node = entry.getKey();
+            visited = entry.getValue();
+
+            if (visited) {
+                k--;
+                if (k == 0) break;
+            } else {
+                // push in R->V->L, then visit in L-V-R          
+                if (node.right != null) {                                                                                   stack.push(new AbstractMap.SimpleEntry(node.right, false));
+                }
+                stack.push(new AbstractMap.SimpleEntry(node, true));
+                if (node.left != null) {
+                    stack.push(new AbstractMap.SimpleEntry(node.left, false));
+                }
+            }
+        }
+
+        return node.val;
+    }
+}
