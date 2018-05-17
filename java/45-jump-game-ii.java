89484cd51cef056ae6ee55a5de6ffb1d38d1e5c8
tianlixu <tianli.xu@gmail.com>
2018-05-17 09:47:35 -0400

    enhanced algorithm, O(n), beatus 95.26

--- java/45-jump-game-ii.java
+++ java/45-jump-game-ii.java
@@ -18,6 +18,44 @@ You can assume that you can always reach the last index.
 */
 
 /**
+   Time: O(n), beats 95.26%
+   Taking a step would give you a window size, once the window_right execeeds the end of the array, you get a solution.
+   Take [2, 3, 1, 1, 4] for example:
+   initilization: window [0, 0]
+       index:       0  1  2  3  4
+       value:       [2], 3, 1, 1, 4
+       window: [0, 0]
+   take 1st step, you get next window [1, 2]: 
+       2, [3, 1], 1, 4
+   take 2nd step, you get next window [3, 4], which reaches the end, you get a solution
+       2, 3, 1, [1, 4]
+ */ 
+
+class Solution {
+    public int jump(int[] nums) {
+        int window_left = 0;
+        int window_right = 0;
+        int steps = 0;
+
+        while (window_right < nums.length - 1) {
+            steps ++;
+            int j = window_right;
+            while (window_left <= j) {
+                window_right = Math.max(window_right, nums[window_left] + window_left);
+                if (window_right >= nums.length - 1) {
+                    break; // reaches the end, no need to continue
+                }
+                window_left ++;
+            }
+        }
+
+        return steps;
+    }
+}
+
+
+
+/**
  * Recursive solution
  * Find the first index that can reach the end then iterate.
  * This is just a solution with long runtime: 215ms
