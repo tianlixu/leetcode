552c0a0939569b9d092646f511eff12e94c93e37
tianlixu <tianli.xu@gmail.com>
2018-05-28 13:58:08 -0400

    DP solution which beats 95

--- py/96-unique-binary-search-trees.py
+++ py/96-unique-binary-search-trees.py
@@ -16,7 +16,35 @@ Given n = 3, there are a total of 5 unique BST's:
      3     2     1      1   3      2
     /     /       \                 \
    2     1         2                 3
+
+DP Solution:
+dp[i] : 1, 2, 3, ..., i
+dp[i+1]: 
+    dp[0] * dp[i]: 1,  2,3,...i,i+1
+  + dp[1] * dp[i-1]: 1,  2,  3,...i,i+1
+  + dp[2] * dp[i-2]: 1,2,  3,  4,...i,i+1
+    ...
+  + dp[i] * dp[0]: 1,2,3,...i,  i+1
 """
+class Solution:
+    def numTrees(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n < 1:
+            return 1
+        
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        
+        for i in range(2, n+1): # for-loop beats 95%, while while-loop beats only 59%
+            for j in range(0, i):
+                dp[i] += dp[j] * dp[i-j-1]
+            
+        return dp[n]
+
 
 # recursive solution,  Time Limit Exceeded
 class Solution:
