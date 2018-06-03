5f111963e1b3220805bcbf4963f883e64ae8c3b4
Alex Xu <tianli.xu@gmail.com>
2018-06-02 21:02:46 -0400

    updated

--- java/1-two-sum.java
+++ java/1-two-sum.java
@@ -2,18 +2,18 @@
  Source : https://oj.leetcode.com/problems/two-sum/
  Author : Alex Xu
  Date   : Jan 16, 2017
+ Problem: Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
  Tag    : array, hashmap
-
- Given an array of integers, find two numbers such that they add up to a specific target number.
-
- The function twoSum should return indices of the two numbers such that they add up to the target,
- where index1 must be less than index2.
- Please note that your returned answers (both index1 and index2) are not zero-based.
-
- You may assume that each input would have exactly one solution.
-
- Input: numbers={2, 7, 11, 15}, target=9
- Output: index1=1, index2=2
 */
 
 import java.util.HashMap;
@@ -21,21 +21,19 @@ import java.util.HashMap;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] ret = new int[2];
-        
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>(); // key: addend, value: index
+        HashMap<Integer, Integer> map = new HashMap<>();
+
         for (int i=0; i<nums.length; i++) {
-            int augend = nums[i];
-            Integer index = map.get(augend);
+            Integer index = map.get(nums[i]);
             if (index == null) {
-                int addend = target - augend;
-                map.put(addend, i);
+                map.put(target - nums[i], i); // key is the 2nd addend, value is its index
             } else {
                 ret[0] = index; // index for the 1st addend
                 ret[1] = i;     // index for the 2nd addend
-                break;
+                break;          // found it, break then
             }
         }
-
+        
         return ret;
     }
 }
