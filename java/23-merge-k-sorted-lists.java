17dbdf4cf1e5a34569ae35058086e03a9da9878d
Alex Xu <tianli.xu@gmail.com>
2018-06-04 22:39:10 -0400

    two java solutions with detail explanation

---
+++ java/23-merge-k-sorted-lists.java
@@ -0,0 +1,139 @@
+/*
+ Source : https://leetcode.com/problems/merge-k-sorted-lists/
+ Author : Alex Xu
+ Date   : June 04, 2018
+ Problem: Merge k sorted linked lists and return it as one sorted list.
+ Analyze and describe its complexity.
+ Example:
+  Input:
+  [
+      1->4->5,
+      1->3->4,
+      2->6
+  ]
+  Output: 1->1->2->3->4->4->5->6
+*/
+
+/*
+Solution 1: 15ms, beats 84.38%
+1. Traverse all the linked lists and collect the values of the nodes into an array.
+2. Sort and iterate over this array to get the proper value of nodes. (quick sort, NlogN)
+3. Create a new sorted linked list and extend it with the new nodes.
+
+
+Complexity Analysis
+
+Time complexity : O(Nlog N) where N is the total number of nodes.
+    - Collecting all the values costs O(N) time.
+    - A stable sorting algorithm costs O(Nlog N) time.
+    - Iterating for creating the linked list costs O(N) time.
+Space complexity : O(N).
+    - Sorting cost O(N) space (depends on the algorithm you choose).
+    - Creating a new linked list costs O(N) space.
+**/
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        /// count total number of nodes in the k lists
+        int N = 0;
+        for (ListNode l : lists) {
+            N += getLength(l);
+        }
+        
+        /// create an array to save all the N nodes
+        int[] nodes = new int[N];
+        int index = 0;
+        for (ListNode l : lists) {
+            while (l != null) {
+                nodes[index] = l.val;
+                index ++;
+                l = l.next;
+            }
+        }
+        
+        /// sort the array nodes
+        Arrays.sort(nodes);
+        
+        /// create the list for N nodes
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        for(int v : nodes) {
+            tail.next = new ListNode(v);
+            tail = tail.next;
+        }
+        
+        return dummy.next;
+    }
+    
+    private int getLength(ListNode list) {
+        int length = 0;
+        while (list != null) {
+            length ++;
+            list = list.next;
+        }
+        
+        return length;
+    }
+}
+
+
+/*
+Solution 2: Accepted, but poor runtime: 436 ms, beats 0%?
+Algorithm:
+    - Compare every k nodes (head of every linked list) and get the node with the smallest value.
+    - Extend the final sorted linked list with the selected nodes.
+
+Complexity Analysis:
+Time complexity : O(kN) where k is the number of linked lists.
+    - Almost every selection of node in final linked costs O(k) (k-1 times comparison).
+    - There are N nodes in the final linked list.
+Space complexity :
+    - O(n) Creating a new linked list costs O(n) space.
+    - O(1) It's not hard to apply in-place method - connect selected nodes instead of creating new nodes to fill the new linked list. (it's O(1) in the following implementation)
+**/
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        int miniIndex = -1;
+        
+        while (true) {
+            for (int i=0; i< lists.length; i++) {
+                if (lists[i] == null) {
+                    continue;
+                }
+
+                if(miniIndex == -1) {
+                    miniIndex = i;
+                    continue;
+                }
+
+                if (lists[miniIndex].val > lists[i].val) {
+                    miniIndex = i;
+                }
+            }
+            
+            if (miniIndex == -1) {
+                break;
+            }
+            
+            tail.next = lists[miniIndex];
+            tail = tail.next;
+            lists[miniIndex] = lists[miniIndex].next;
+            miniIndex = -1;
+        }
+        
+        
+        return dummy.next;
+    }
+}
+
+
+/*
+ continue with solution 2:
+ 1. for the 1st comparison of the heads nodes of the k lists, we can sore them out, so we get the
+   1.1 the smallest node, and
+   1.2 a sorted k-1 nodes
+
+ 2. for the next N-1 comparison, use inesrt search logN
+
+ */
