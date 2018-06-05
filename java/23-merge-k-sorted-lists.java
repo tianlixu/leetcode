4c93bfab40cd01498a9f20e0c3129427e503c706
tianlixu <tianli.xu@gmail.com>
2018-06-05 11:46:11 -0400

    solution 3 using PriorityQueue

--- java/23-merge-k-sorted-lists.java
+++ java/23-merge-k-sorted-lists.java
@@ -129,11 +129,68 @@ class Solution {
 
 
 /*
- continue with solution 2:
- 1. for the 1st comparison of the heads nodes of the k lists, we can sore them out, so we get the
+Solution 3: 
+ continue with solution 2: Runtime 83ms, beats 7.91%
+ 1. for the 1st comparison of the heads nodes of the k lists, we can sore them out, so we get
    1.1 the smallest node, and
    1.2 a sorted k-1 nodes
 
- 2. for the next N-1 comparison, use inesrt search logN
+ 2. for the next N-1 comparison, use insert sort, Time is O(logk)
 
+Complexity Analysis
+
+Time complexity : O(Nlog k) where k is the number of linked lists.
+    - The comparison cost will be reduced to O(logk) for every pop and insertion to priority queue. 
+      But finding the node with the smallest value just costs O(1) time.
+    - There are N nodes in the final linked list.
+Space complexity :
+    - O(n) Creating a new linked list costs O(n) space.
+    - O(k) The code above present applies in-place method which cost O(1) space. And the priority queue (often implemented with heaps) costs O(k) space (it's far less than N in most situations).
+
+
+Note that: the following declaration of pq has runtime 21ms and beats 57%?! All the remaining part is the same.
+
+        PriorityQueue<ListNode> queue= new PriorityQueue<ListNode>(lists.length,new Comparator<ListNode>(){
+            @Override
+            public int compare(ListNode o1,ListNode o2){
+                if (o1.val<o2.val)
+                    return -1;
+                else if (o1.val==o2.val)
+                    return 0;
+                else 
+                    return 1;
+            }
+        });
  */
+import java.util.PriorityQueue;
+
+class Solution {   
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+      
+        // create priority queue and add the head nodes of the k lists to pq
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(lists.length, (l1, l2) -> l1.val - l2.val);
+        for (ListNode l : lists) {
+            if (l != null) {
+                pq.add(l);
+            }
+        }
+        
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while (!pq.isEmpty()) {
+            ListNode node = pq.poll(); // get the smallest and link it to tail
+            tail.next = node;
+            tail = tail.next;
+            if (node.next != null) {
+                pq.add(node.next); // add smallest.next to pq
+            }
+        }
+        tail.next = null;
+        
+        return dummy.next;
+    }
+}
+
