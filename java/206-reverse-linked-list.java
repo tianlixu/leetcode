9fa88c9b8c537e5cf8ad4d359ddeb4c2df2f4f1d
tianlixu <tianli.xu@gmail.com>
2018-06-12 10:04:50 -0400

    added recursive solution

--- java/206-reverse-linked-list.java
+++ java/206-reverse-linked-list.java
@@ -38,4 +38,17 @@ class Solution {
     }
 }
 
-
+// Recursive solution: Runtime 2ms, beats 3.81%
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        
+        return newHead;
+    }
+}
